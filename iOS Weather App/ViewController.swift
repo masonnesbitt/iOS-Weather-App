10c13dc3fdc49dc500bb511265675db7b2b70b44
Mason Nesbitt <masonnesbitt@macbook-pro-10.local>
2018-10-25 09:52:22 -0500

    Initial Commit

---
+++ iOS Weather App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS Weather App
+//
+//  Created by Mason Nesbitt on 10/25/18.
+//  Copyright © 2018 Mason Nesbitt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
