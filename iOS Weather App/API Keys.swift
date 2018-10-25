b3136c2a2a90d88a8711f616447c740ca580a004
Mason Nesbitt <masonnesbitt@macbook-pro-10.local>
2018-10-25 12:39:26 -0500

    API Initt

---
+++ iOS Weather App/API Keys.swift
@@ -0,0 +1,14 @@
+//
+//  API Keys.swift
+//  iOS Weather App
+//
+//  Created by Mason Nesbitt on 10/25/18.
+//  Copyright © 2018 Mason Nesbitt. All rights reserved.
+//
+
+import Foundation
+
+struct APIKeys {
+    let darkSkyKey = " "
+    let googleKey = " "
+}
