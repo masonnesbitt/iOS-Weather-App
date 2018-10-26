9d4c76d5b07e641ef510fb06a129ea2042a39f5d
Mason Nesbitt <masonnesbitt@macbook-pro-10.local>
2018-10-26 13:03:39 -0500

    Setup

--- iOS Weather App/API Keys.swift
+++ iOS Weather App/API Keys.swift
@@ -13,7 +13,13 @@ import SwiftyJSON
 
 struct APIKeys {
     let darkSkyKey = "eaa29835ef76d87aeedeace2178f3ce8"
+    
+    // https://api.darksy.net/forecast/ + API Key + / + Latitude + , + Longitude
+    
     let googleKey = "AIzaSyAguRJsvwhmb1z3RKFODQb9x9u_ubX0M5g"
+    
+    //https://maps.googleapis.com/maps/api/geocode/json?address= + Address + &key= + API Key
 }
+
 
 
